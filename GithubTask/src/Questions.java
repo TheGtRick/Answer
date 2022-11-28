c4b11f6b99349f1eeeba31130577bb284903dda3
unknown <raiymbekzhanuzak@gmail.com>
2022-11-29 01:05:21 +0600

    Task

---
+++ GithubTask/src/Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Raiymbek");
+        System.out.println("Surname: Zhanuzak");
+        System.out.println("Id: 220107020");
+        System.out.println("Hobby: Sculpting");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("Am I the best teacher?: YES!!!!!");
+    }
+}
